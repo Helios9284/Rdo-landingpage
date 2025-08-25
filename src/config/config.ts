ca343e1e0ef1805c3c926a571b25c30fbb93fecf
helios9284 <colonelme9284@gmail.com>
2025-08-25 11:29:04 -0400

    update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://rdo-landingpage-ha3a.vercel.app/'
+export const BASE_URL = 'https://rdo-landingpage-ha3a-git-master-helios9284s-projects.vercel.app/'
 export const API_URL = 'http://127.0.0.1:3000/api'
